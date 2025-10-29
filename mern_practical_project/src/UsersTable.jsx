7417f25803f33550bdd5abccee29516862c98e68
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-29 23:02:35 +0530

    Update UsersTable.jsx

--- mern_practical_project/src/UsersTable.jsx
+++ mern_practical_project/src/UsersTable.jsx
@@ -13,7 +13,7 @@ const UsersTable = ({rows}) => {
         <TableBody>
             {
                 rows.map(row =>(
-                    <TableRow key={}>
+                    <TableRow key={row.id} sx={{'&:last-child td, &:last-child th':{border: 0}}}>
                         <TableCell component='th' scope="row">{row.id}</TableCell>
                     </TableRow>
                     

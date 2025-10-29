0f65684f210d4ea8827e2c26a35f731912ef3837
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-29 23:16:37 +0530

    Update UsersTable.jsx

--- mern_practical_project/src/UsersTable.jsx
+++ mern_practical_project/src/UsersTable.jsx
@@ -1,7 +1,8 @@
-import { TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 const UsersTable = ({rows}) => {
- <TableContainer component={paper}>
+    return(
+         <TableContainer component={Paper}>
     <Table>
         <TableHead>
             <TableRow>
@@ -12,16 +13,39 @@ const UsersTable = ({rows}) => {
         </TableHead>
         <TableBody>
             {
-                rows.map(row =>(
+                rows.length > 0 ? rows.map(row =>(
                     <TableRow key={row.id} sx={{'&:last-child td, &:last-child th':{border: 0}}}>
                         <TableCell component='th' scope="row">{row.id}</TableCell>
+                        <TableCell component='th' scope="row">{row.name}</TableCell>
+                        <TableCell>
+                            <Button
+                                sx={{margin: '0px 10px'}}
+                                onClick={() =>{}}
+                            >
+                                Update
+                            </Button>
+                            <Button
+                                sx={{margin: '0px 10px'}}
+                                onClick={() =>{}}
+                            >
+                                Delete
+                            </Button>
+                        </TableCell>
                     </TableRow>
                     
-                ))
+                )):(
+                    <TableRow  key={row.id} sx={{'&:last-child td, &:last-child th':{border: 0}}}>
+                    <TableCell component='th' scope="row" colSpan={3} align="center">
+                        No Users Found
+                    </TableCell>
+                    </TableRow>
+                )
             }
         </TableBody>
     </Table>
  </TableContainer>
+    )
+
 }
 
 export default UsersTable;

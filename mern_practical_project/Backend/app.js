6570f650f95ce7934ccd83666c7e027ebaf33941
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-30 17:12:02 +0530

    Update app.js

--- mern_practical_project/Backend/app.js
+++ mern_practical_project/Backend/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const controller = require('./controller')
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get('/users', (req, res) =>{
+    controller.getUsers(users =>{
+        res.send(users);
+    })
+});
+
+app.get('/user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserById(id, user => {
+        res.send(user);
+    });
+});
 
 module.exports = app;

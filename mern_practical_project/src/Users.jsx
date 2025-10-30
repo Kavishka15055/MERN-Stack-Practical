2015977e0112d0ef3e809884b71512e300a46065
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-30 23:54:58 +0530

    Update Users.jsx

--- mern_practical_project/src/Users.jsx
+++ mern_practical_project/src/Users.jsx
@@ -4,35 +4,85 @@ import UsersTable from "./UsersTable";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 
-
-
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
 
-  useEffect(() =>{
+  useEffect(() => {
     getUsers();
-  },[]);
+  }, []);
 
   const getUsers = () => {
     Axios.get("http://localhost:3001/api/users")
-      .then(response => {
+      .then((response) => {
         setUsers(response.data?.response || []);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Axios error:", error);
       });
-  }
-  
+  };
+
+  const addUser = (data) => {
+    setSubmitted(true);
+    const payload = {
+      id: data.id,
+      name: data.name,
+    };
+
+    Axios.post("http://localhost:3001/api/createuser", payload)
+      .then(() => {
+        getUsers();
+        setSubmitted(false);
+        setIsEdit(false);
+      })
+      .catch((error) => {
+        console.error("Axios error:", error);
+      });
+  };
+
+  const updateUser = (data) => {
+    setSubmitted(true);
+
+    const payload = {
+      id: data.id,
+      name: data.name,
+    };
+
+    Axios.post("http://localhost:3001/api/updateuser", payload)
+      .then(() => {
+        getUsers();
+        setSubmitted(false);
+        setIsEdit(false);
+      })
+      .catch((error) => {
+        console.error("Axios error:", error);
+      });
+  };
+
   return (
     <Box
       sx={{
-        width:'calc(100% - 100px)',
-        margin: 'auto',
-        marginTop: '100px',
+        width: "calc(100% - 100px)",
+        margin: "auto",
+        marginTop: "100px",
       }}
     >
-      <UserForm />
-      <UsersTable rows={users}/>
+      <UserForm
+        addUser={addUser}
+        updateUser={updateUser}
+        submitted={submitted}
+        data={selectedUser}
+        isEdit={isEdit}
+      />
+      <UsersTable
+        rows={users}
+        selectedUser={(data) => {
+          setSelectedUser(data);
+          setIsEdit(true);
+        }}
+      />
     </Box>
   );
 };

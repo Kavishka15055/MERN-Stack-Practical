a1cce09ce532263ee8d2060f3c2685a528a8f92e
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-30 20:54:57 +0530

    Update controller.js

--- mern_practical_project/Backend/controller.js
+++ mern_practical_project/Backend/controller.js
@@ -1,22 +1,54 @@
-const users = [
-    {
-    id: 1,
-    name: 'Kavishka'
-    },
-    {
-    id: 2,
-    name: 'Nimesh'
-    }
-    
-];
-const getUsers = (cb) =>{
-    cb(users);
+const { response } = require("./app");
+const User = require("./model");
+
+const getUsers = (req, res, next) => {
+  User.find()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ message: "An error occurred" });
+    });
 };
 
-const getUserById = (id, cb) =>{
-    const user = users.find(user => user.id === id);
-    cb(user);
-}
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ message: "An error occurred" });
+    });
+};
+
+const updateUser = (req, res, next) => {
+  const { id, name } = req.body;
+  User.updateOne({ id: id }, { $set: { name: name } })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ message: "An error occurred" });
+    });
+};
+
+const deleteUser = (req, res, next) => {
+  const id = req.body.id;
+  User.deleteOne({ id: id })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ message: "An error occurred" });
+    });
+};
 
 exports.getUsers = getUsers;
-exports.getUserById = getUserById;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;

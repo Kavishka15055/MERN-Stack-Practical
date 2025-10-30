0b4a00cd7ceb7c5dee07a5f6fd2d04263fc9bb80
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-30 22:45:08 +0530

    Update UsersTable.jsx

--- mern_practical_project/src/UsersTable.jsx
+++ mern_practical_project/src/UsersTable.jsx
@@ -1,51 +1,71 @@
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
-const UsersTable = ({rows}) => {
-    return(
-         <TableContainer component={Paper}>
-    <Table>
+const UsersTable = ({ rows }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
         <TableHead>
-            <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Actions</TableCell>
-            </TableRow>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
         </TableHead>
-        <TableBody>
-            {
-                rows.length > 0 ? rows.map(row =>(
-                    <TableRow key={row.id} sx={{'&:last-child td, &:last-child th':{border: 0}}}>
-                        <TableCell component='th' scope="row">{row.id}</TableCell>
-                        <TableCell component='th' scope="row">{row.name}</TableCell>
-                        <TableCell>
-                            <Button
-                                sx={{margin: '0px 10px'}}
-                                onClick={() =>{}}
-                            >
-                                Update
-                            </Button>
-                            <Button
-                                sx={{margin: '0px 10px'}}
-                                onClick={() =>{}}
-                            >
-                                Delete
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                    
-                )):(
-                    <TableRow  key={row.id} sx={{'&:last-child td, &:last-child th':{border: 0}}}>
-                    <TableCell component='th' scope="row" colSpan={3} align="center">
-                        No Users Found
-                    </TableCell>
-                    </TableRow>
-                )
-            }
-        </TableBody>
-    </Table>
- </TableContainer>
-    )
 
-}
+        <TableBody>
+          {rows.length > 0 ? (
+            rows.map((row) => (
+              <TableRow
+                key={row.id || row._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    onClick={() => {
+                      // Update user logic here
+                      console.log("Update", row);
+                    }}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    color="error"
+                    onClick={() => {
+                      // Delete user logic here
+                      console.log("Delete", row);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={3} align="center">
+                No Users Found
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default UsersTable;

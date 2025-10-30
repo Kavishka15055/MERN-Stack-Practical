d1389d2a09bac8a374d21cc09efeba2dd27b1670
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-30 20:55:21 +0530

    Update server.js

--- mern_practical_project/Backend/server.js
+++ mern_practical_project/Backend/server.js
@@ -1,7 +1,30 @@
-const app = require('./app');
+// const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = 3001;
-const host = '127.0.0.1';
+const host = 'localhost';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://kavishkapiyumal:hueI4FBZyDAapqEr@cluster0.8zxcmnr.mongodb.net/?appName=Cluster0'; // MongoDB connection string
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    }
+    catch(error) {
+        console.error('Error connecting to MongoDB');
+    }
+};
+
+connect();
 
 const server = app.listen(port,host, () =>{
     console.log(`Node server is listening to ${server.address().port}`)
 });
+
+app.use('/api', router);
